91bea8be705a3e4e9166ca6e1c8682253626c3d2
matzkel <matzkel@proton.me>
2024-08-21 13:10:29 +0300

    Add nuitka specific options for compilation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
+# Nuitka specific compilation options
+# nuitka-project-if: {OS} in ("Windows"):
+#   nuitka-project: --onefile
+#   nuitka-project: --mingw64
+#   nuitka-project: --lto=yes
+#   nuitka-project: --disable-console
+#   nuitka-project: --output-dir=build\windows
+#   nuitka-project: --output-filename=pyfncm
+# nuitka-project-else:
+#   nuitka-project: --onefile
+#   nuitka-project: --lto=yes
+#   nuitka-project: --output-dir=build/linux
+#   nuitka-project: --output-filename=pyfncm
+
+
 def main():
     print("Hello, world!")
 
