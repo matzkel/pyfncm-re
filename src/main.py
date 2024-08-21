5c245654fee3c41e46fdf3be3db34bd15fe9fabe
matzkel <matzkel@proton.me>
2024-08-21 22:50:22 +0200

    Add logging and nuitka compilation options

--- src/main.py
+++ src/main.py
@@ -2,18 +2,21 @@
 # nuitka-project-if: {OS} in ("Windows"):
 #   nuitka-project: --onefile
 #   nuitka-project: --mingw64
+#   nuitka-project: --enable-plugin=pyside6
 #   nuitka-project: --lto=yes
 #   nuitka-project: --disable-console
 #   nuitka-project: --output-dir=build\windows
 #   nuitka-project: --output-filename=pyfncm
 # nuitka-project-else:
 #   nuitka-project: --onefile
+#   nuitka-project: --enable-plugin=pyside6
 #   nuitka-project: --lto=yes
 #   nuitka-project: --output-dir=build/linux
 #   nuitka-project: --output-filename=pyfncm
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
+from logger import getLogger
 from profile_widget import ProfileWidget
 
 
@@ -28,7 +31,11 @@ if __name__ == "__main__":
     """ Run the application. """
     import sys
 
+    logger = getLogger("main.py")
+
     app = QApplication([])
+    logger.info("Application started...")
+
     main_window = MainWindow(ProfileWidget())
     main_window.show()
     sys.exit(app.exec())
