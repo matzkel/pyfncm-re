0d71bf739cc1b18017356350c51ac826a2f673c6
matzkel <matzkel@proton.me>
2024-08-21 22:07:14 +0200

    Add logging

---
+++ src/logger.py
@@ -0,0 +1,39 @@
+import logging
+import os
+from datetime import datetime
+
+
+def getLogger(name):
+    """Return a logger with the specified name."""
+
+    # Logging hierarchy below:
+    # logs/
+    #   2024/
+    #     January/
+    #       2024-01-01.log
+    #       2024-01-02.log
+    #       ...
+    #     February/
+    #       ...
+    #   2025/
+    #     ...
+
+    current_date = datetime.now()
+    LOG_PATH = os.path.join(
+        "logs",
+        os.path.join(
+            str(current_date.year),
+            str(current_date.strftime("%B")),
+        ),
+    )
+    os.makedirs(LOG_PATH, exist_ok=True)
+
+    logger = logging.getLogger(name)
+    logging.basicConfig(
+        filename=os.path.join(LOG_PATH, f"{current_date.strftime("%Y-%m-%d")}.log"),
+        encoding="utf-8",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.DEBUG,
+    )
+
+    return logger
