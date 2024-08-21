4ab856e091c28f00f410d5b0f3f6eeaef5cacb64
matzkel <matzkel@proton.me>
2024-08-21 11:44:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
+
+# Check if ./venv exists
+DIRECTORY="./venv"
+if [ ! -d "$DIRECTORY" ]; then
+    python -m venv venv
+
+    # Check if ./requirements.txt exist and install them
+    REQUIREMENTS="./requirements.txt"
+    if [ -d "$REQUIREMENTS" ]; then
+        python -m pip install -r requirements.txt
+    fi
+fi
+
+# Start the application
+source ./venv/bin/activate
 python ./src/main.py
