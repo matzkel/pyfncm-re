718b8fe48e761692ff1ea5f0330960088235c600
matzkel <matzkel@proton.me>
2024-08-21 13:36:35 +0300

    Update build scripts to download needed dependencies

--- build.bat
+++ build.bat
@@ -1,11 +1,23 @@
-Rem Before building run ./start.bat script atleast once
-
 Rem Windows
 Rem Sadly nuitka doesn't support cross-compilation so you have to 
 Rem setup virtual machine, install python 3.12 and use it to compile the app
 
 Rem mingw64 toolchain is REQUIRED to compile the application on Windows
 Rem python is REQUIRED to be add to your system's PATH environment variable
+@echo off
+
+Rem Check if ./venv exists
+set DIRECTORY=venv
+if not exists DIRECTORY {
+    python -m venv venv
+
+    Rem Check if ./requirements.txt exist and install them
+    set REQUIREMENTS=requirements.txt
+    if exists REQUIREMENTS {
+        call venv\Scripts\activate.bat
+        python -m pip install -r requirements.txt
+    }
+}
 
 Rem Build for Windows
 call venv\Scripts\activate.bat
--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-# Before building run ./start.sh script atleast once
-
 # Linux
 # patchelf is REQUIRED to create standalone application on linux
 # ccache is OPTIONAL, but recommended for faster building speed
+
+# Check if ./venv exists
+DIRECTORY="./venv"
+if [ ! -d "$DIRECTORY" ]; then
+    python -m venv venv
+
+    # Check if ./requirements.txt exist and install them
+    REQUIREMENTS="./requirements.txt"
+    if [ -f "$REQUIREMENTS" ]; then
+        source ./venv/bin/activate
+        python -m pip install -r ./requirements.txt
+    fi
+fi
 
 # Build for Linux
 source ./venv/bin/activate
