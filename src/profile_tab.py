d5050476869fbe5430b930817856db94e9bae34e
matzkel <matzkel@proton.me>
2024-08-26 17:56:24 +0200

    Add placeholder code

--- src/profile_tab.py
+++ src/profile_tab.py
@@ -1,4 +1,11 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import (
+    QAbstractItemView,
+    QHBoxLayout,
+    QPushButton,
+    QTableWidget,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class ProfileTab(QWidget):
@@ -6,3 +13,26 @@ class ProfileTab(QWidget):
 
     def __init__(self):
         super().__init__()
+
+        top_bar = QHBoxLayout()
+        delete_profile = QPushButton("Supprimer le profil")
+        search = QPushButton("Recherche")
+        top_bar.addWidget(delete_profile, 4)
+        top_bar.addWidget(search, 12)
+
+        table = QTableWidget()
+        table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        table.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        bottom_bar = QHBoxLayout()
+        add_order = QPushButton("Ajouter une commande")
+        delete_order = QPushButton("Supprimer la commande")
+        bottom_bar.addWidget(add_order)
+        bottom_bar.addWidget(delete_order)
+
+        layout = QVBoxLayout(self)
+        layout.addLayout(top_bar)
+        layout.addWidget(table)
+        layout.addLayout(bottom_bar)
+
+        self.setLayout(layout)
